57e60397637ead2da59b4e199b4c5ba9fb7747e6
foollingn <113342040+foollingn@users.noreply.github.com>
2022-09-13 21:20:46 +0300

    git push

--- helper.py
+++ helper.py
@@ -4,6 +4,7 @@ import re
 
 #Json file upload.
 def loadjson(filepath):
+    print(1)
     try:
         with open(filepath, 'r', encoding='utf-8') as jsonfile:
             return json.load(jsonfile)
