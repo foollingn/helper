11809432a26bf849102e243d21a9d6be45bbc919
foollingn <113342040+foollingn@users.noreply.github.com>
2022-09-19 21:13:53 +0300

    git push

--- helper.py
+++ helper.py
@@ -4,17 +4,22 @@ import json
 import random
 import re 
 
-debug = True
+
 colorama.init(autoreset=True)
+
+class DebHel(object):
+    debug = True
+
+if DebHel.debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}Helper is  successfully loaded")
 
 #Loading to json file.
 def loadjson(filepath):
     try:
         with open(filepath, 'r', encoding='utf-8') as jsonfile:
-            if debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(loadjson): Loading to Json File '{filepath}'")
+            if DebHel.debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(loadjson): Loading to Json File '{filepath}'")
             return json.load(jsonfile)
     except Exception as e:
-        if debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(loadjson): Errored ' {e} '")
+        if DebHel.debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(loadjson): Errored ' {e} '")
         return None
 
 #Write to json file.
@@ -28,9 +33,9 @@ def dumpjson(data, filepath):
             else: pop=pop+1
         with open(filepath, 'w', encoding='utf-8') as file:
             json.dump(datas, file, indent=4, ensure_ascii=False)
-            if debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(dumpjson): Successfully dumping data '{datas}'")
+            if DebHel.debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(dumpjson): Successfully dumping data '{datas}'")
     except Exception as e:
-        if debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(dumpjson): Errored ' {e} '")
+        if DebHel.debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(dumpjson): Errored ' {e} '")
         return None
 
 #Adding a new dictionary to json.
@@ -40,9 +45,9 @@ def push(filepath, data):
         json_data.append(data)
         with open(filepath, 'w', encoding='utf-8') as outfile:
             json.dump(json_data, outfile, indent=4, ensure_ascii=False)
-            if debug: print(f"{Fore.YELLOW} DEBUG HELPER | {Fore.MAGENTA}(push): Successfully pushing in {filepath} by data '{json_data}'")
+            if DebHel.debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(push): Successfully pushing in {filepath} by data '{json_data}'")
     except Exception as e:
-        if debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(push): Errored ' {e} '")
+        if DebHel.debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(push): Errored ' {e} '")
         return None
 
 #Delete to json file.
@@ -57,19 +62,19 @@ def delete(filepath, id):
                 minimal = minimal + 1
         with open(filepath, 'w', encoding='utf-8') as outfile:
             json.dump(data, outfile, ensure_ascii=False, indent=4)
-            if debug: print(f"{Fore.YELLOW} DEBUG HELPER | {Fore.MAGENTA}(delete): Successfully deleted user with ID is {id}")
+            if DebHel.debug: print(f"{Fore.YELLOW} DEBUG HELPER | {Fore.MAGENTA}(delete): Successfully deleted user with ID is {id}")
             return True
     except Exception as e:
-        if debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(delete): Errored ' {e} '")
+        if DebHel.debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(delete): Errored ' {e} '")
         return None
         
 #Beautiful numbers.
 def task(intenger):
     try:
-        if debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(task): Successfully tasked")
+        if DebHel.debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(task): Successfully tasked")
         return re.sub(r'(?<!^)(?=(\d{3})+$)', r'.', f"{intenger}")
     except Exception as e:
-        if debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(task): Errored ' {e} '")
+        if DebHel.debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(task): Errored ' {e} '")
         return None
 
 #Maximum id.
@@ -77,10 +82,10 @@ def maxuId(filepath):
     try:
         data = loadjson(filepath)
         max_item = max(data, key = lambda item: int(item['uid']))
-        if debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(maxuId): Successfully found the last number")
+        if DebHel.debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(maxuId): Successfully found the last number")
         return int(max_item['uid'])+1
     except Exception as e:
-        if debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(maxuId): Errored ' {e} '")
+        if DebHel.debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(maxuId): Errored ' {e} '")
         return 0
 
 #Random by chance.
@@ -90,7 +95,7 @@ def chance(chance, x=bool):
         if status is True: x = [1, 2, 2.5, 5, 10]
         else: x = [0, 0.5, 0.25, 0.75]
         totalx = random.choice(x)
-        if debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(chance): Total is {status}, ({totalx}x)")
+        if DebHel.debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(chance): Total is {status}, ({totalx}x)")
         return {"status": status, "bet": totalx}
     return {"status": status}
 
@@ -99,10 +104,10 @@ def replace(string):
     try:
         text = string.replace('к', '000')
         text = text.replace('k', '000')
-        if debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(replace): Befor is {string}, After is {int(text)}")
+        if DebHel.debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(replace): Befor is {string}, After is {int(text)}")
         return int(text)
     except Exception as e:
-        if debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(replace): Errored ' {e} '")
+        if DebHel.debug: print(f"{Fore.RED}DEBUG HELPER | {Fore.MAGENTA}(replace): Errored ' {e} '")
         return 0
 
 #Find to user by id.
@@ -110,5 +115,5 @@ def find(object, id, filepath):
     try:
         return list(filter(lambda x: x[object]==id, loadjson(filepath)))[0]
     except Exception as e:
-        if debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(find): finding is user with ID is {id} by object {object}")
+        if DebHel.debug: print(f"{Fore.YELLOW}DEBUG HELPER | {Fore.MAGENTA}(find): finding is user with ID is {id} by object {object}")
         return None
--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 from vkbottle.bot import Bot, Message
 from vkbottle import BaseMiddleware
-import helper
+from helper import *
 from loguru import logger
-
 logger.disable("vkbottle")
 bot = Bot(token="vk1.a.ASL_RbsdqEYCsgqFCRruLvgZl8uZX1EPYoSEqDwyxSeqXoP6c5CXlFh7lhoYiwj9Utx0EhowlyQ5RN3Egu15CFFzmCAZih-1u3Qoe2X3EK4O5Bo6eCtV1qDm1e2kdXpw0Rv4eRYD716DiZjk2v9USuA-7u44Jbtt3qeyyHYTlWTKOyjNmrAIVgBIeOqr_oGb")
 users = "users.json"
 
 class InfoMiddleware(BaseMiddleware[Message]):
     async def pre(self):
-        user = helper.find("id", self.event.from_id, users)
+        user = find("id", self.event.from_id, users)
         users_info = await bot.api.users.get(self.event.from_id)
         if not user:
-            helper.push(users, {
+            push(users, {
                 "id": self.event.from_id,
-                "uid": helper.maxuId(users),
+                "uid": maxuId(users),
                 "nick": users_info[0].first_name,
                 "balance": 1000,
                 "message": 1
             })
-            await self.event.answer
+            await self.event.answer("куда мы лезим...")
 @bot.on.message(text=["проф", "профиль"])
 async def handler(ans: Message):
-    user = helper.find("id", ans.from_id, users)
-    text = f"{user['nick']}, ну ты конечно прокаченый тип:\n"
-    text += f"твой ид: {user['uid']}"
-    text += f"твой кровный балик: {user['balance']}"
-    text += f"скок ты настрочил смск: {user['message']}"
+    user = find("id", ans.from_id, users)
+    text = f"{user['nick']}, ну ты конечно прокаченый тип:\n\n"
+    text += f"твой ид: {user['uid']}\n"
+    text += f"твой кровный балик: {task(user['balance'])}\n"
+    text += f"скок ты настрочил смск: {user['message']}\n"
     return text
 
 bot.labeler.message_view.register_middleware(InfoMiddleware)
